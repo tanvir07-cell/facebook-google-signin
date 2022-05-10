2d7fe15bb73a05cfb41d4fd14a672017bf34c32f
unknown <tr9836859@gmail.com>
2022-05-10 11:31:40 +0600

    facebook and google sigin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { FaGoogle, FaFacebook } from "react-icons/fa";
+import "./App.css";
+import app from "./firebase.init";
+import {
+  FacebookAuthProvider,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
+
+const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+  const googleProvider = new GoogleAuthProvider();
+  const facebookProvider = new FacebookAuthProvider();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log("Error : ", error);
+      });
+  };
+
+  const handleFacebookSignIn = () => {
+    signInWithPopup(auth, facebookProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error("Error : ", error);
+      });
+  };
+
+  const handleSingOut = () => {
+    signOut(auth)
+      .then((result) => {
+        const user = result?.user;
+        setUser({});
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error("Error : ", error);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.email ? (
+        <button onClick={handleSingOut}>Sign Out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSignIn}>
+            <FaGoogle></FaGoogle> signIn
+          </button>
+
+          <button onClick={handleFacebookSignIn}>
+            <FaFacebook></FaFacebook> signIn
+          </button>
+        </>
+      )}
+
+      <h1>Name : {user.displayName}</h1>
+      <p>Email : {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB25QRzrZFbGxpEAEd8ZkZBiQ6VfDXIODQ",
+  authDomain: "simple-firebase-auth2-3425a.firebaseapp.com",
+  projectId: "simple-firebase-auth2-3425a",
+  storageBucket: "simple-firebase-auth2-3425a.appspot.com",
+  messagingSenderId: "811860107742",
+  appId: "1:811860107742:web:5f73d154a4190fbb1cbd4f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
